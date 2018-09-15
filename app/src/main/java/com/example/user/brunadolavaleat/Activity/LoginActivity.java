49da0aa652d70295db1f75dcbf990f23dfd41a2f
BrunaDolavale <brunadolavalerj@gmail.com>
2018-09-15 00:10:55 -0300

    IP Configura 2a parte do firebase

--- app/src/main/java/com/example/user/brunadolavaleat/Activity/LoginActivity.java
+++ app/src/main/java/com/example/user/brunadolavaleat/Activity/LoginActivity.java
@@ -1,15 +1,32 @@
 package com.example.user.brunadolavaleat.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.user.brunadolavaleat.Activity.Entities.User;
+import com.example.user.brunadolavaleat.DAO.ConfigFirebase;
 import com.example.user.brunadolavaleat.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editEmail;
+    private EditText editSenha;
+    private Button btnFazerLogin;
+    private FirebaseAuth authentication;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +35,39 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editSenha = (EditText) findViewById(R.id.editSenha);
+        btnFazerLogin = (Button) findViewById(R.id.btnFazerLogin);
+    }
+
+    public void getEmailAndPassword(View view) {
+        if (!editEmail.getText().toString().equals("") && !editSenha.getText().toString().equals("")) {
+            user = new User();
+            user.setEmail(editEmail.getText().toString());
+            user.setSenha(editSenha.getText().toString());
+            validateLogin();
+
+        } else {
+            Toast.makeText(LoginActivity.this, "Preencha", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void validateLogin(){
+        authentication = ConfigFirebase.getFireBaseAuthentication();
+        authentication.signInWithEmailAndPassword(user.getEmail(), user.getSenha()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    openMainView();
+                    Toast.makeText(LoginActivity.this, "Login Efetuado", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    public void openMainView(){
+        Intent intentOpenMainView = new Intent(LoginActivity.this, ListActivity.class);
+        startActivity(intentOpenMainView);
     }
 
 }
--- app/src/main/java/com/example/user/brunadolavaleat/DAO/ConfigFirebase.java
+++ app/src/main/java/com/example/user/brunadolavaleat/DAO/ConfigFirebase.java
@@ -1,5 +1,26 @@
 package com.example.user.brunadolavaleat.DAO;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ConfigFirebase {
 
+    private static DatabaseReference databaseReference;
+
+    private static FirebaseAuth autentication;
+
+    public static DatabaseReference getFirebase(){
+        if (databaseReference == null){
+            databaseReference = FirebaseDatabase.getInstance().getReference();
+        }
+        return databaseReference;
+    }
+
+    public static FirebaseAuth getFireBaseAuthentication(){
+        if (autentication == null) {
+            autentication = FirebaseAuth.getInstance();
+        }
+        return autentication;
+    }
 }
