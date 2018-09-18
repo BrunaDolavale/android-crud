9435f2bc91fd697c949f765ca6a657644de01879
BrunaDolavale <brunadolavalerj@gmail.com>
2018-09-18 18:15:31 -0300

    Adiciona primeira parte fb

--- app/src/main/java/com/example/user/brunadolavaleat/Activity/LoginActivity.java
+++ app/src/main/java/com/example/user/brunadolavaleat/Activity/LoginActivity.java
@@ -20,6 +20,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class LoginActivity extends AppCompatActivity {
 
