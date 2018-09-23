d2923ee61b7a57e3b05c13caa929cfef2b7aaedc
BrunaDolavale <brunadolavalerj@gmail.com>
2018-09-23 15:39:39 -0300

    Adiciona Hash facebook

--- app/src/main/java/com/example/user/brunadolavaleat/Activity/LoginActivity.java
+++ app/src/main/java/com/example/user/brunadolavaleat/Activity/LoginActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,12 +17,18 @@ import android.widget.Toast;
 import com.example.user.brunadolavaleat.Activity.Entities.User;
 import com.example.user.brunadolavaleat.DAO.ConfigFirebase;
 import com.example.user.brunadolavaleat.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -31,6 +38,8 @@ public class LoginActivity extends AppCompatActivity {
     private TextView edtAbreCadastro;
     private FirebaseAuth authentication;
     private User user;
+    private Button loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +48,36 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        FacebookSdk.getApplicationContext();
+
         editEmail = (EditText) findViewById(R.id.editEmail);
         editSenha = (EditText) findViewById(R.id.editSenha);
         btnFazerLogin = (Button) findViewById(R.id.btnFazerLogin);
         edtAbreCadastro = (TextView) findViewById(R.id.edtAbreCadastro);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();
     }
+
+
+//    // Callback registration
+//    loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//        @Override
+//        public void onSuccess(LoginResult loginResult) {
+//            // App code
+//        }
+//
+//        @Override
+//        public void onCancel() {
+//            // App code
+//        }
+//
+//        @Override
+//        public void onError(FacebookException exception) {
+//            // App code
+//        }
+//    });
+
+
 
     private void validateLogin(){
         authentication = ConfigFirebase.getFireBaseAuthentication();
@@ -80,7 +114,5 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
         startActivity(intent);
     }
-
-
 
 }
